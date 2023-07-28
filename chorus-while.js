e0ee19dd1cddbc936feda1516a09ab4b19e553a8
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-07-28 14:24:11 -0600

    Practicing committing

--- chorus-while.js
+++ chorus-while.js
@@ -2,7 +2,9 @@ const chorus = "Let's dance!";
 let repeat = 0;
 while (repeat < 10) {
   if (repeat === 5) {
-    console.log(chorus);
-    repeat++;
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
 }
-console.log("Until the sun comes up!");}
+console.log("Until the sun comes up!");
