d7d9902ee88a51760e5497b2908eeebede33d14c
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-07-29 00:15:05 -0600

    Assignments

---
+++ arrays.js
@@ -0,0 +1,14 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+
+films[2] = "Jurassic Park";
+films;
+films.push("A.I.");
+films;
+
+"My favourite " + director + " film is " + films[1];
+
+"films at index 0: " + films[0];
+"films at index 1: " + films[1];
+"films at index 2: " + films[2];
+"films at index 3: " + films[3];
---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+// Add the flavour "root beer" to the array without modifying the original array
+const newIceCreamFlavours = iceCreamFlavours.concat("root beer");
+console.log(newIceCreamFlavours);
+
+// Get the first flavour in the array
+console.log(iceCreamFlavours[0]);
+
+// Get the last flavour in the array
+console.log(newIceCreamFlavours[newIceCreamFlavours.length - 1]);
+
+// Get the total number of flavours in the array
+console.log(newIceCreamFlavours.length);
